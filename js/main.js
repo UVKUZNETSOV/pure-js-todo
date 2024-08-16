cc34b34b02ab7137f11424cbbcd205538bfd468a
UVKUZNETSOV <yuriy_kuznetsov_73@mail.ru>
2024-08-16 13:55:09 +0300

    UPD

--- js/main.js
+++ js/main.js
@@ -2,7 +2,35 @@ const input = document.querySelector(".form-input");
 const submitButton = document.querySelector(".form-button");
 const todoInner = document.querySelector(".todo-inner");
 
-const todoArr = [];
+let todoArr = [];
+
+if (localStorage.getItem("todos")) {
+  todoArr = JSON.parse(localStorage.getItem("todos"));
+}
+
+for (key in todoArr) {
+  const task = document.createElement("div");
+  const text = document.createElement("p");
+  const deleteButton = document.createElement("button");
+
+  text.innerHTML = todoArr[key];
+
+  todoInner.insertAdjacentElement("beforeend", task);
+  task.insertAdjacentElement("beforeend", text);
+  task.insertAdjacentElement("beforeend", deleteButton);
+
+  task.addEventListener("click", () => {
+    task.classList.toggle('completed');
+  }) 
+
+  console.log(todoArr)
+
+  deleteButton.addEventListener("click", () => {
+    deleteButton.closest("div").remove();
+    todoArr = todoArr.filter(todo => todo !== todoArr[key]);
+    localStorage.setItem("todos", JSON.stringify(todoArr))
+  })
+}
 
 const postTask = todo => {
   const task = document.createElement("div");
@@ -24,6 +52,7 @@ const postTask = todo => {
   }) 
 
   deleteButton.addEventListener("click", () => {
+    console.log("OK")
     deleteButton.closest("div").remove();
   })
 }
