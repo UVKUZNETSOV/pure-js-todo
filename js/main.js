06775c38f6acaf8fc1d0272fe82642efc006f5e1
UVKUZNETSOV <yuriy_kuznetsov_73@mail.ru>
2024-08-18 22:36:46 +0300

    UPD

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const input = document.querySelector(".form-input");
 const submitButton = document.querySelector(".form-button");
 const todoInner = document.querySelector(".todo-inner");
+const deleteAllButton = document.querySelector(".delete-all-btn");
+const markAllAsCompleted = document.querySelector(".mark-as-complited");
 
 let todoArr = [];
 
@@ -16,7 +18,7 @@ const render = () => {
   
     deleteButton.classList.add("delete-btn");
   
-    text.innerHTML = todoArr[key];
+    text.innerHTML = todoArr[key].todo;
   
     todoInner.insertAdjacentElement("beforeend", task);
     task.insertAdjacentElement("beforeend", text);
@@ -24,13 +26,30 @@ const render = () => {
   
     let temp = deleteButton.closest("div").getElementsByTagName("p")[0];
   
-    temp.classList.add(`${key}`);
+    temp.setAttribute("id", `${key}`);
+    temp.classList.add("todo");
+
+    if (todoArr[key].completed) {
+      temp.classList.toggle("completed");
+    }
   }
 
   const deleteButton = document.querySelectorAll(".delete-btn");
+  const todo = document.querySelectorAll(".todo");
+
+  todo.forEach(todo => {
+    todo.addEventListener("click", () => {
+      todo.classList.toggle("completed");
+      todoArr[todo.id].completed = !todoArr[todo.id].completed;
+      localStorage.setItem("todos", JSON.stringify(todoArr));
+      todoInner.innerHTML = '';
+      render();
+    })
+  })
+
 
   deleteButton.forEach(btn => {
-    const temp = parseInt(btn.closest("div").getElementsByTagName("p")[0].className);
+    const temp = parseInt(btn.closest("div").getElementsByTagName("p")[0].id);
     btn.addEventListener("click", () => {
       todoArr.splice(temp, 1);
       localStorage.setItem("todos", JSON.stringify(todoArr));
@@ -45,10 +64,26 @@ render();
 submitButton.addEventListener("click", e => {
   e.preventDefault();
   if (input.value) {
-    todoArr.push(input.value);
+    todoArr.push({todo: input.value, completed: false});
     localStorage.setItem("todos", JSON.stringify(todoArr));
     todoInner.innerHTML = '';
     input.value = "";
     render();
   }
 })
+
+deleteAllButton.addEventListener("click", () => {
+  todoInner.innerHTML = '';
+  todoArr = [];
+  localStorage.setItem("todos", todoArr);
+})
+
+markAllAsCompleted.addEventListener("click", () => {
+  for (key in todoArr) {
+    todoArr[key].completed = true;
+    localStorage.setItem("todos", JSON.stringify(todoArr));
+    todoInner.innerHTML = '';
+    render();
+  }
+})
+
