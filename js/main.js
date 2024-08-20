8fde63b4ce5f687d6814286a424693abb6463d82
UVKUZNETSOV <yuriy_kuznetsov_73@mail.ru>
2024-08-20 11:26:17 +0300

    UPD

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ const submitButton = document.querySelector(".form-button");
 const todoInner = document.querySelector(".todo-inner");
 const deleteAllButton = document.querySelector(".delete-all-btn");
 const markAllAsCompleted = document.querySelector(".mark-as-complited");
+const confirmation = document.querySelector(".confirmation");
+const confirmBtn = document.querySelector(".confirmation-btn");
 
 let todoArr = [];
 
@@ -20,6 +22,8 @@ const render = (translate, id) => {
     const deleteButton = document.createElement("button");
   
     deleteButton.classList.add("delete-btn");
+
+    deleteButton.innerHTML = '<svg width="24"height="24"viewBox="0 0 24 24"fill="none"xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd"clip-rule="evenodd"d="M17 6V5C17 3.89543 16.1046 3 15 3H9C7.89543 3 7 3.89543 7 5V6H4C3.44772 6 3 6.44772 3 7C3 7.55228 3.44772 8 4 8H5V19C5 20.6569 6.34315 22 8 22H16C17.6569 22 19 20.6569 19 19V8H20C20.5523 8 21 7.55228 21 7C21 6.44772 20.5523 6 20 6H17ZM15 5H9V6H15V5ZM17 8H7V19C7 19.5523 7.44772 20 8 20H16C16.5523 20 17 19.5523 17 19V8Z"fill="currentColor"/></svg>'
   
     text.innerHTML = todoArr[key].todo;
   
@@ -31,7 +35,9 @@ const render = (translate, id) => {
   
     temp.setAttribute("id", `${key}`);
     temp.classList.add("todo");
+
     task.setAttribute("id", `${key}`);
+    task.setAttribute("draggable", "true");
 
     if (todoArr[key].completed) {
       temp.classList.toggle("completed");
@@ -57,7 +63,14 @@ const render = (translate, id) => {
 
   deleteButton.forEach(btn => {
     const temp = parseInt(btn.closest("div").getElementsByTagName("p")[0].id);
+    const elem = btn.closest("div").getElementsByTagName("p")[0];
     btn.addEventListener("click", () => {
+      confirmation.style.display = "flex";
+      localStorage.setItem("queue", elem);
+      setTimeout(() => {
+        confirmation.style.display = "none";
+        localStorage.removeItem("queue");
+      }, 3000)
       todoArr.splice(temp, 1);
       localStorage.setItem("todos", JSON.stringify(todoArr));
       btn.closest("div").classList.add("disappear");
